f8c064fb71a87f635c5ad0c1aae717ab99dba728
Omar Peña <opena854@gmail.com>
2021-04-03 20:11:54 -0400

    no funca

--- src/config/config.js
+++ src/config/config.js
@@ -31,7 +31,13 @@ const config = {
     PageNotFound: lazy(() => import('../pages/PageNotFound/PageNotFound')),
   },
   firebase: {
-
+    apiKey: "AIzaSyCfHb4xd5K2BLjfnoR0ar7YRJABo8wPyFw",
+    authDomain: "millo-do.firebaseapp.com",
+    projectId: "millo-do",
+    storageBucket: "millo-do.appspot.com",
+    messagingSenderId: "600382027183",
+    appId: "1:600382027183:web:476b4cea7548f3a799ab5a",
+    measurementId: "G-QJDYVRK7VM"
   },
 }
 
