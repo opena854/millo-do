be24a6a62c9a57f473eb8a04b70521d93cbca029
Omar Peña <opena854@gmail.com>
2022-03-21 02:31:41 -0400

    on save, go to list.

--- src/pages/third.tsx
+++ src/pages/third.tsx
@@ -1,20 +1,26 @@
 import { Typography } from "@mui/material"
 import { Fragment } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import Form from "../components/form"
 import ThirdModel from "../model/third"
 import { useDocument } from "../services/store"
 
 const Third = () => {
   const { id } = useParams<"id">();
+  const navigate = useNavigate();
+
   const [data, loading, saveDocument] = useDocument(ThirdModel.dbPath, id === "new" ? undefined : id);
+  const onSubmit = saveDocument ? async (data: any) => {
+    await saveDocument(data);
+    navigate("/thirds")
+  } : undefined
 
   return (
     <Fragment>
       <Typography variant="h4" mb={2}>
         Editar Tercero
       </Typography>
-      {loading ? <div>Loading...</div> : <Form document={data} fields={ThirdModel.formFields} onSubmit={saveDocument} />}
+      {loading ? <div>Loading...</div> : <Form document={data} fields={ThirdModel.formFields} onSubmit={onSubmit} />}
     </Fragment>
   );
 }
